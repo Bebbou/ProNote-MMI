b6e25fb947ed7b2a10d7ad319ad898760d8df361
Bebou <lino.volle.dev@gmail.com>
2025-11-24 13:23:05 +0100

    Update scprit.js

--- rel/scrpit/scprit.js
+++ rel/scrpit/scprit.js
@@ -1,10 +1,10 @@
 function changeAgenda(group) {
 
   const calendarLinks = {
-        A1: "https://calendar.google.com/calendar/embed?src=q1vn93bgrhpo43pkugu7fjafpqcjjhd4%40import.calendar.google.com&ctz=Europe%2FParis",
-        A2: "https://calendar.google.com/calendar/embed?src=esgp5ljnaq8kkkhmqj8qtdmhrdh4ae2p%40import.calendar.google.com&ctz=Europe%2FParis",
-        B1: "https://calendar.google.com/calendar/embed?src=eckdovv9sc1o0lrjbspc8jh960boq694%40import.calendar.google.com&ctz=Europe%2FParis",
-        B2: "https://calendar.google.com/calendar/embed?src=soj89lsmodb18bvqv7m27qo1btcot9r9%40import.calendar.google.com&ctz=Europe%2FParis"
+        A1: "https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FParis&mode=WEEK&showTz=0&showPrint=0&src=cTF2bjkzYmdyaHBvNDNwa3VndTdmamFmcHFjampoZDRAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23b39ddb",
+        A2: "https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FParis&showPrint=0&showTz=0&mode=WEEK&src=ZXNncDVsam5hcThra2tobXFqOHF0ZG1ocmRoNGFlMnBAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23f09300",
+        B1: "https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FParis&showPrint=0&showTz=0&mode=WEEK&src=ZWNrZG92djlzYzFvMGxyamJzcGM4amg5NjBib3E2OTRAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23f6bf26",
+        B2: "https://calendar.google.com/calendar/embed?height=600&wkst=2&ctz=Europe%2FParis&showPrint=0&showTz=0&mode=WEEK&src=c29qODlsc21vZGIxOGJ2cXY3bTI3cW8xYnRjb3Q5cjlAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23a79b8e"
     };
 
     document.getElementById("agenda-frame").src = calendarLinks[group];
@@ -15,4 +15,5 @@ function openPopup(id) {
 
 function closePopup(id) {
     document.getElementById(id).style.display = "none";
+
 }
